558b8cc67c2f00bad06c656de73c38e2a997d016
Fastor1us <o-kir@ro.ru>
2026-04-06 16:52:29 +0300

    feat: validate event model chore: readme.md

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventApi.Models;
 
-public abstract class EventBase
+public abstract class EventBase : IValidatableObject
 {
+    [Required]
     public required string Title { get; set; }
     public string? Description { get; set; }
+    [Required]
     public required DateTime StartAt { get; set; }
+    [Required]
     public required DateTime EndAt { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndAt <= StartAt)
+        {
+            yield return new ValidationResult(
+                "EndAt must be later than StartAt",
+                [nameof(EndAt)]
+            );
+        }
+    }
 }
 
 public class EventRequestDto : EventBase { }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# event-service
+# Event API
+
+Простое REST API для управления событиями CRUD-операциями
+
+## 📋 Требования
+
+- [.NET 10.0](https://dotnet.microsoft.com/download/dotnet/10.0)
+- CLI / IDE
+
+## 🚀 Быстрый старт
+
+- Восстановление зависимостей
+```bash
+dotnet restore
+```
+- Сборка проекта
+```bash
+dotnet build
+```
+- Запуск приложения
+```bash
+dotnet run
+```
+
+## 🧪 Тестирование
+После запуска приложения через команду <code>dotnet run</code> доступен 
+[Swagger UI](http://localhost:5142/swagger/index.html)
